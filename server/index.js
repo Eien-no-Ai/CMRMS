f18dc917342c46e7f3fc7eca032b707a825e4134
JaysonDelos <97875801+JaysonDelos@users.noreply.github.com>
2024-10-25 20:39:35 +0800

    package update and delete

--- server/index.js
+++ server/index.js
@@ -260,6 +260,64 @@ app.get("/api/packages/:id", async (req, res) => {
   }
 });
 
+app.delete("/api/packages/:id", (req, res) => {
+  const { id } = req.params;
+  PackageModel.findByIdAndDelete(id)
+    .then((package) => {
+      if (package) {
+        res.json({ message: "Patient deleted successfully" });
+      } else {
+        res.status(404).json({ message: "Patient not found" });
+      }
+    })
+    .catch((err) =>
+      res.status(500).json({ message: "Error deleting patient", error: err })
+    );
+});
+
+app.put("/api/packages/:id", (req, res) => {
+  const { id } = req.params;
+  const updatedPackages = req.body;
+
+  PackageModel.findByIdAndUpdate(id, updatedPackages, { new: true })
+    .then((package) => {
+      if (package) {
+        res.json({ message: "Patient updated successfully", package });
+      } else {
+        res.status(404).json({ message: "Patient not found" });
+      }
+    })
+    .catch((err) =>
+      res.status(500).json({ message: "Error updating patient", error: err })
+    );
+});
+
+// app.put("/api/packages/:id", async (req, res) => {
+//   const { id } = req.params;
+//   const updatedPackage = req.body;
+
+//   try {
+//     const package = await PackageModel.findByIdAndUpdate(id, updatedPackage, { new: true });
+//     res.json(package);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: "Error updating package" });
+//   }
+// });
+
+// app.delete("/api/packages/:id", async (req, res) => {
+//   const { id } = req.params;
+//   try {
+//     await PackageModel.findByIdAndDelete(id);
+//     res.json({ message: "Package deleted successfully" });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: "Error deleting package" });
+//   }
+// });
+
+
+
 // P H Y S I C A L   T H E R A P Y   R E C O R D S
 
 app.post("/api/physicalTherapy", async (req, res) => {
